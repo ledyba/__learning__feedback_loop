029f15c2fabc34b6a6a5f81a44b28c92573b4ab4
Kaede Fujisaki <kaede@hexe.net>
2020-05-31 05:30:39 +0900

    Use more common term

--- src/engine/Controller.ts
+++ src/engine/Controller.ts
@@ -2,12 +2,12 @@ import { Block } from "./System";
 import { ChartDataSets } from "chart.js";
 
 export class ProportionalBlock implements Block {
-  private weight: number;
-  constructor(weight: number) {
-    this.weight = weight;
+  private gain: number;
+  constructor(gain: number) {
+    this.gain = gain;
   }
   step(at: number, dt: number, input: number): number {
-    return input * this.weight;
+    return input * this.gain;
   }
   get inspect(): ChartDataSets[] {
     return []
