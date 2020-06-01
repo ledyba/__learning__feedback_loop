9ba329d0fa5381d49a727543259cdd5d88b50296
Kaede Fujisaki <kaede@hexe.net>
2020-06-02 06:03:57 +0900

    add Integral controller

--- src/engine/Controller.ts
+++ src/engine/Controller.ts
@@ -2,7 +2,7 @@ import { Block } from "./System";
 import { ChartDataSets } from "chart.js";
 
 export class ProportionalBlock implements Block {
-  private gain: number;
+  private readonly gain: number;
   constructor(gain: number) {
     this.gain = gain;
   }
@@ -11,5 +11,24 @@ export class ProportionalBlock implements Block {
   }
   get inspect(): ChartDataSets[] {
     return []
+  }
+}
+
+export class IntegralController implements Block {
+  private sum: number;
+  private priv: number;
+  private readonly gain: number;
+  constructor(gain: number) {
+    this.sum = 0;
+    this.priv = 0;
+    this.gain = gain;
+  }
+  step(at: number, dt: number, input: number): number {
+    this.sum += (this.priv + input) * dt / 2.0;
+    this.priv = input;
+    return this.sum * this.gain;
+  }
+  get inspect(): ChartDataSets[] {
+    return [];
   }
 }
