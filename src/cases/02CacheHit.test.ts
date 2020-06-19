882e8a04537e7eadf4b67212df44e7c7151521d3
Kaede Fujisaki <kaede@hexe.net>
2020-06-19 14:38:44 +0900

    Passed all tests

--- src/cases/02CacheHit.test.ts
+++ src/cases/02CacheHit.test.ts
@@ -33,12 +33,26 @@ test('test LRU', () => {
   expect(cache.getEntry(3)).toBe(false);
   expect(cache.getEntry(3)).toBe(true);
   // id=1 is last used!
+  expect(cache.has(1)).toBe(false);
   expect(cache.getEntry(1)).toBe(false);
   expect(cache.getEntry(1)).toBe(true);
-  expect(cache.getEntry(3)).toBe(false);
-  expect(cache.getEntry(3)).toBe(true);
-  // id=2 is last used!
+  expect(cache.has(2)).toBe(false);
   expect(cache.getEntry(2)).toBe(false);
   expect(cache.getEntry(2)).toBe(true);
+  // id=3 is last used!
+  expect(cache.getEntry(3)).toBe(false);
+  expect(cache.getEntry(3)).toBe(true);
 });
 
+test('fuzzing', () => {
+  const cache = new Cache(10);
+  const lasts = new Array(10);
+  for(let i = 0; i < 1000; ++i) {
+    const id = Math.floor(Math.random() * 1000);
+    lasts[i % 10] = id;
+    cache.getEntry(id);
+  }
+  for(let id = 0; id < 1000; ++id) {
+    expect(cache.has(id)).toBe(lasts.indexOf(id) >= 0);
+  }
+});
--- src/cases/02CacheHit.ts
+++ src/cases/02CacheHit.ts
@@ -1,8 +1,7 @@
 import { ChartDataSets, ChartData } from 'chart.js';
 import { constantSetpoint, invert, defaultOutput, System, Block, DataRecorder, connect, loop, mix } from "../engine/System";
 import { ProportionalBlock, IntegralController } from '../engine/Controller';
-import { assert, timeStamp } from 'console';
-import { throws } from 'assert';
+import { assert } from 'console';
 
 /**
  * Cache hit!
@@ -24,6 +23,9 @@ export class Cache {
   constructor(size: number) {
     this.maxSize_ = size;
   }
+  public has(id: number): boolean {
+    return this.map.get(id) !== undefined;
+  }
 
   public getEntry(id: number): boolean {
     const prev = this.map.get(id);
@@ -34,44 +36,69 @@ export class Cache {
         this.first.next = entry;
       }
       this.first = entry;
-      if(!this.last) {
+      if(this.last == null) {
         this.last = entry;
       }
       if(this.map.size > this.maxSize_) {
         // delete the last used entry
+        if(!this.last) {
+          fail("There are no entry to remove.");
+        }
+        this.map.delete(this.last.id);
+        this.last = this.last.next;
         if(this.last) {
-          this.map.delete(this.last.id);
-          this.last = this.last.next;
-          if(this.last !== null) {
-            this.map.set(this.last.id, null);
+          this.map.set(this.last.id, null);
+        } else {
+          if(this.map.size !== 0) {
+            fail(`Next last is null, but hashmap is not empty: ${this.map.size}`);
           }
+          this.first = null;
         }
       }
       return false;
     } else if(prev === null) {
-      assert(this.last?.id === id);
+      if(this.last === null) {
+        fail('Hashmap indicates that the entry is last, but last was null.');
+      }
+      if(this.first === this.last) {
+        // already on the first, because there are just only one entry.
+        assert(this.map.size === 1);
+        assert(this.first.id === id);
+        return true;
+      }
+      const nextLast = this.last.next;
+      if(nextLast) {
+        this.map.set(nextLast.id, null);
+      }
+      this.map.set(this.last.id, this.first);
+      this.last.next = null;
+      if(this.first) {
+        this.first.next = this.last;
+      }
+      this.last = nextLast;
       return true;
     } else {
-      this.pushFront(prev);
-      return true;
-    }
-  }
-
-  private pushFront(prev: CacheEntry) {
-    const entry = prev.next;
-    if(entry === null) {
-      assert(entry !== null);
-      return;
-    }
-    const next = entry.next;
-    prev.next = next;
-    if(next) {
-      this.map.set(prev.id, next);
-    }
-    this.map.set(entry.id, this.first);
-    if(this.first) {
-      this.first.next = entry;
+      const entry = prev.next;
+      if(entry === null) {
+        fail('prev.next can not be null. Entry is not the front.');
+      }
+      if(this.first === null) {
+        fail('this.first cannot be null. Cache is not empty.');
+      }
+      if(this.first === entry) {
+        // It's already first.
+        assert(this.first.next === null, "the first.next should be always null.");
+        return true;
+      }
+      assert(entry.next, "entry.next should be not null, if it's not the front.");
+      const next = entry.next!;
+      prev.next = next;
+      this.map.set(next.id, prev);
       this.map.set(entry.id, this.first);
+      this.first.next = entry;
+      this.first = entry;
+      this.first.next = null;
+      return true;
     }
   }
 
