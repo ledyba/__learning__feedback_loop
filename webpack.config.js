bb6eab1e964d5cdc3ab95ca0aa03de72a3afe851
Kaede Fujisaki <kaede@hexe.net>
2020-10-23 08:04:36 +0900

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,28 @@ const path = require('path');
 module.exports = {
   mode: 'development', // "production" | "development" | "none"
   entry: {
-    'main': __dirname+'/src/main.ts',
+    'main': path.join(__dirname, 'src', 'main.ts'),
   },
   output: {
-    path: __dirname + '/dist/',
+    path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     publicPath: '/dist/'
   },
   module: {
-      rules: [{
-      // 拡張子 .ts の場合
-      test: /\.ts$/,
-      // TypeScript をコンパイルする
-      use: 'ts-loader'
-      }]
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
   },
   resolve: {
-      modules: [
-      "node_modules", // node_modules 内も対象とする
-      ],
-      extensions: [
-      '.ts',
-      '.js' // node_modulesのライブラリ読み込みに必要
-      ]
+    modules: [
+    "node_modules", // node_modules 内も対象とする
+    ],
+    extensions: [
+    '.ts',
+    '.js' // node_modulesのライブラリ読み込みに必要
+    ]
   }
 };
